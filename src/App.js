1709ec40d724c7ec8bcacd0cf275442099c63772
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-07-25 14:42:02 +0530

    Using Constructor define states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import './App.css'
 import Navbar from './Component/layout/Navbar'
+import Useritem from './Component/user/Useritem'
 
 import './App.css';
 class App extends Component {
@@ -13,8 +14,8 @@ class App extends Component {
     <div className="App">
 
       <Navbar/>
-      
-      
+
+      <Useritem/>
     </div>
 
     );
