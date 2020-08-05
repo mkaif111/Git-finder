c24a04f124ae3083efdf820566297093603c39f2
Kaif <36125283+thekaif7@users.noreply.github.com>
2020-08-06 00:30:31 +0530

    Http Request and state updatng  and axios.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React,{Component} from 'react';
 import './App.css'
 import Navbar from './Component/layout/Navbar'
 import User from './Component/user/User'
+import axios from 'axios'
 
 import './App.css';
 class App extends Component {
+  state = {
+    user: [],
+    loading : false
+  }
+
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({user:res.data , loading:false});
+    console.log(res.data);
+  }
    
   render()
   {
@@ -15,7 +27,7 @@ class App extends Component {
 
       <Navbar/>
       <div className="container">
-      <User/>
+      <User loading={this.state.loading} users={this.state.user} />
       </div>
       
     </div>
